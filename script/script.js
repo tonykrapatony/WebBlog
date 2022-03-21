9aec20d73a5bb9be0337f4c42f5c699c0567754f
Ihor <tonykrpatony@gmail.com>
2022-03-21 18:21:14 +0200

    swapi + feedback

--- script/script.js
+++ script/script.js
@@ -6,36 +6,41 @@ $(document).ready(function(){
         slidesToScroll: 1,
         dots: true
     });
-
-    // news
-    const url = 'https://newsapi.org/v2/everything?q=bitcoin&apiKey=69ccb163c2624d7ab8698ab9f1e1b8a6';
-
-    $.getJSON( url, function( data ) {
-        console.log(data);
-        for(let key in data.articles){
-            let title = data.articles[key].title;
-            let description = ""
-            if (data.articles[key].description != null) {
-                description = data.articles[key].description;
-            } else {
-                description = "";
-            }
-            let img = "";
-            if (data.articles[key].urlToImage != null) {
-                img = data.articles[key].urlToImage;
-            } else {
-                img = `./img/tech${getRandomArbitrary(1, 3)}.jpg`;
-            }
-            let result = `<div class="news-item"><h2>${title}</h2><p>${description}</p><img src="${img}" alt="News image"><a href="${data.articles[key].url}">More</a></div>`;
-            console.log(result);
-            $('.news').append(result);
-        }
-      });
 });
 
 function getRandomArbitrary(min, max) {
     return Math.floor(min + Math.random() * (max + 1 - min));
-}
+} 
+let result;
+function showChar() {
+    let ep = document.getElementById('ep');
+    let ep_num = ep.value;
+    const url = `https://swapi.dev/api/people/?page=${ep_num}`;  
+    $.getJSON( url, function( data ) {
+        for(let key in data.results){
+            let name = data.results[key].name;
+            let height = data.results[key].height;
+            let mass = data.results[key].mass;
+            let gender = data.results[key].gender;
+            let char = `<div class="sw-item"><h2>${name}</h2><p>Height: ${height}</p><p>Mass: ${mass}</p><p>Gender: ${gender}</p></div>`;
+            result += char;
+            $('.sw').html(result);
+        }
+    });
+};
+
+document.querySelector('.show-char').addEventListener('click', function(){
+    if (result != "") {
+        result = "";
+        console.log(result);
+        showChar();
+    } else {
+        showChar();
+    }
+});
+
+
+
 
 let show_btn = document.querySelectorAll('.slide-btn');
 for(let i=0; i<=show_btn.length; i++){
@@ -46,4 +51,5 @@ for(let i=0; i<=show_btn.length; i++){
         document.querySelector('.article-wrap').style.display = 'none';
     })
 }
+
 
