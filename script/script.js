7d80c3c785a53a9f033a023edc0cb8d4bcdbfc70
Ihor <tonykrpatony@gmail.com>
2022-04-02 13:46:43 +0300

    add a toogle button fordark and light theme

--- script/script.js
+++ script/script.js
@@ -1,5 +1,21 @@
-document.querySelector('.burger').addEventListener('click', function(){
+// burger button and show/hide menu
+let burger = document.querySelector('.burger'),
+    burgItem1 = document.querySelector('.burger div');
+burger.addEventListener('click', function(){
     let elDisp = window.getComputedStyle(document.querySelector('.nav__menu')).display;
+    if (!burger.classList.contains('burger-anim1') && !burger.classList.contains('burger-anim2')){
+        burger.classList.add('burger-anim1');
+        burgItem1.style.display = 'none';
+    } else if (burger.classList.contains('burger-anim1') && !burger.classList.contains('burger-anim2')){
+        burger.classList.remove('burger-anim1');
+        burger.classList.add('burger-anim2');
+        let foo = () => burgItem1.style.display = 'block';
+        setTimeout(foo, 2500);
+    } else {
+        burgItem1.style.display = 'none';
+        burger.classList.add('burger-anim1');
+        burger.classList.remove('burger-anim2');
+    }
     if (elDisp === 'none') {
         document.querySelector('.nav').style.animation = 'bgToBig 2s ease-in-out forwards';
         document.querySelector('.nav__menu').style.display = 'flex';
@@ -13,8 +29,7 @@ document.querySelector('.burger').addEventListener('click', function(){
         setTimeout(hideMenu, 2000);
     }
 });
-
-
+// slider
 $(document).ready(function(){
     // slider
     $('.slider').slick({
@@ -24,6 +39,8 @@ $(document).ready(function(){
         dots: true,
         arrows: true,
         speed: 300,
+        autoplay: true,
+        autoplaySpeed: 5000,
         responsive: [
             {
             breakpoint: 426,
@@ -38,6 +55,7 @@ $(document).ready(function(){
 function getRandomArbitrary(min, max) {
     return Math.floor(min + Math.random() * (max + 1 - min));
 } 
+// show starwars characters
 let result;
 function showChar() {
     let ep = document.getElementById('ep');
@@ -55,7 +73,6 @@ function showChar() {
         }
     });
 };
-
 document.querySelector('.show-char').addEventListener('click', function(){
     if (result != "") {
         result = "";
@@ -66,14 +83,16 @@ document.querySelector('.show-char').addEventListener('click', function(){
     }
 });
 
-let firstName, secondName, phone, email, text, newWin;
-
+// 
+let firstName, secondName, phone, email, text, newWin, url;
 function closeWin(a){
     a.close();
 }
 function sendMessage(){
-    newWin = window.open("https://api.telegram.org/bot1529048680:AAGrVc1FwHn5itl5N3MS472eth_ibfrfG1w/sendMessage?chat_id=-648756262&text="+firstName+"          "+secondName+"          "+phone+"          "+email+"          "+text, "_blank");
-    setTimeout(closeWin, 100, newWin);
+    url = "https://api.telegram.org/bot1529048680:AAGrVc1FwHn5itl5N3MS472eth_ibfrfG1w/sendMessage?chat_id=-648756262&text="+firstName+"        "+secondName+"        "+phone+"        "+email+"        "+text;
+    let xhttp = new XMLHttpRequest();
+    xhttp.open("GET", url, true);
+    xhttp.send(); 
 }
 
 function validation(){
@@ -82,7 +101,7 @@ function validation(){
     phone = document.querySelector('.form-phone').value,
     email = document.querySelector('.form-email').value,
     text = document.querySelector('.form-text').value;
-    let namePattern = /[A-z]|[a-z]/;
+    let namePattern = /[A-z]|[a-z]|[А-Я]|[а-я]/;
         phonePattern = /[^\d]/;
         emailPattern = /^[^ ]+@[^ ]+\.[a-z]{2,3}$/;
     if (firstName === ""){
@@ -105,20 +124,111 @@ function validation(){
         sendMessage();
     }
 }
-
 document.querySelector('.form-button').addEventListener('click', function(){
     validation();
 });
 
+let checkbox = document.querySelector('.toggle-button'),
+body = document.querySelector('body'),
+    nav = document.querySelector('.nav'),
+    navLogo = document.querySelector('.nav_logo img'),
+    navLogoSpan = document.querySelector('.nav_logo span'),
+    navMenu = document.querySelectorAll('.nav__menu-item'),
+    toggleLight = document.querySelector('.toggleLight'),
+    toggleDark = document.querySelector('.toggleDark'),
+    swContainer = document.querySelector('.sw-container'),
+    swSelect = document.querySelector('.episode select'),
+    showCharT = document.querySelector('.show-char'),
+    fbForm = document.querySelector('.fb-form'),
+    gitLogo = document.querySelector('.git img'),
+    linkedinLogo = document.querySelector('.linkedin img'),
+    instLogo = document.querySelector('.inst img'),
+    fbLogo = document.querySelector('.fb img'),
+    burgerStick = document.querySelectorAll('.burger div');
+function lightTheme() {
+    body.classList.remove('bg-dark');
+    body.classList.add('bg-light');
+    body.classList.add('bg-light');
+    nav.classList.remove('bg-dark');
+    nav.classList.add('bg-light');
+    navLogo.setAttribute('src', './img/logo2.png');
+    navLogoSpan.classList.add('bg-light');
+    navLogoSpan.classList.remove('bg-dark');
+    navMenu.forEach(element => {
+        element.classList.remove('bg-dark');
+        element.classList.add('bg-light');
+    });
+    toggleLight.classList.add('disp-none');
+    toggleDark.classList.remove('disp-none');
+    swContainer.classList.remove('bg-dark');
+    swContainer.classList.add('bg-light');
+    swSelect.classList.remove('border-dark');
+    swSelect.classList.add('border-light');
+    showCharT.classList.remove('bg-dark');
+    showCharT.classList.remove('border-dark');
+    showCharT.classList.add('bg-light');
+    showCharT.classList.add('border-light');
+    fbForm.classList.remove('fb-form-dark');
+    fbForm.classList.add('fb-form-light');
+    gitLogo.setAttribute('src', './img/github_1.png');
+    linkedinLogo.setAttribute('src', './img/linkedin_1.png');
+    instLogo.setAttribute('src', './img/instagram_1.png');
+    fbLogo.setAttribute('src', './img/facebook_1.png');
+    burgerStick.forEach(el => {
+        el.style.backgroundColor = 'rgb(12, 12, 12)';
+    });
+}
 
+function darkTheme() {
+    body.classList.remove('bg-light');
+    body.classList.add('bg-dark');
+    nav.classList.remove('bg-light');
+    nav.classList.add('bg-dark');
+    navLogo.setAttribute('src', './img/logo1.png');
+    navLogoSpan.classList.add('bg-dark');
+    navLogoSpan.classList.remove('bg-light');
+    navMenu.forEach(element => {
+        element.classList.add('bg-dark');
+        element.classList.remove('bg-light');
+    });
+    toggleLight.classList.remove('disp-none');
+    toggleDark.classList.add('disp-none');
+    swContainer.classList.remove('bg-light');
+    swContainer.classList.add('bg-dark');
+    swSelect.classList.remove('border-light');
+    swSelect.classList.add('border-dark');
+    showCharT.classList.remove('bg-light');
+    showCharT.classList.remove('border-light');
+    showCharT.classList.add('bg-dark');
+    showCharT.classList.add('border-dark');
+    fbForm.classList.remove('fb-form-light');
+    fbForm.classList.add('fb-form-dark');
+    gitLogo.setAttribute('src', './img/github.png');
+    linkedinLogo.setAttribute('src', './img/linkedin.png');
+    instLogo.setAttribute('src', './img/instagram.png');
+    fbLogo.setAttribute('src', './img/facebook.png');
+    burgerStick.forEach(el => {
+        el.style.backgroundColor = 'rgb(184, 205, 219)';
+    });
+}
+
+checkbox.addEventListener('change', function() {
+    if (this.checked) {
+        lightTheme();
+    } else {
+        darkTheme();
+    }
+})
 let show_btn = document.querySelectorAll('.slide-btn');
-for(let i=0; i<=show_btn.length; i++){
-    show_btn[i].addEventListener('click', function(){
+show_btn.forEach(element => {
+    element.addEventListener('click', function(){
         document.querySelector('.article-wrap').style.display = 'block';
+        document.body.style.overflow = 'hidden'
     })
     document.querySelector('.close-btn').addEventListener('click', function(){
         document.querySelector('.article-wrap').style.display = 'none';
+        document.body.style.overflow = 'auto'
     })
-}
+});
 
 
